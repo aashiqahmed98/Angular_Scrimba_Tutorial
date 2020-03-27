e31fdc1ad2cc7ed58c9b4de39f120fbd28f72adb
AashiqinCode <aashiqahmed10101@gmail.com>
2020-03-27 22:41:04 +0530

    Tweaks on Customer-list

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 import {ICustomer} from '../../shared/interfaces';
 
@@ -8,13 +8,21 @@ import {ICustomer} from '../../shared/interfaces';
     templateUrl: './customers-list.component.html'
 })
 export class CustomersListComponent implements OnInit {
+    private _customers: ICustomer[] = [];
+
+    @Input() get customers(): ICustomer[] {
+        return this._customers;
+    }
+    
+    set customers(value: ICustomer[]) {
+        if (value) {
+            this.filteredCustomers = this._customers = value;
+            this.calculateOrders();
+        }
+    }
 
     filteredCustomers: any[] = [
-        { id: 1, name: 'john Doe', city: 'Phoenix', orderTotal: 9.99, customerSince: new Date(2014, 7, 10) },
-        { id: 2, name: 'Jane Doe', city: 'Chandler', orderTotal: 19.99, customerSince: new Date(2017, 2, 22)},
-        { id: 3, name: 'Michelle Thomas', city: 'Seattle', orderTotal: 99.99, customerSince: new Date(2002, 10, 31)},
-        { id: 4, name: 'Jim Thomas', city: 'New York', orderTotal: 599.99, customerSince: new Date(2002, 10, 31)},
-    
+       
     ];
     customersOrderTotal: number;
     currencyCode: string = 'USD';
